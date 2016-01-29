c859c39eb46e4d9fb8788a87cd9383bce764ae4e
John Bellone <jbellone@bloomberg.net>
2016-01-29 12:13:39 -0500

    Remove the unused variable/search call.

--- recipes/kubelet.rb
+++ recipes/kubelet.rb
@@ -5,12 +5,6 @@
 # Copyright 2015-2016, Bloomberg Finance L.P.
 #
 
-unless Chef::Config[:solo]
-
-  cluster_proxy = search(:node, 'tags:"kubernetes.clusterproxy"').first
-
-end
-
 service 'kubelet' do
   action :enable
 end
