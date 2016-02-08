ae0f04311e0f5630fb8348cd29b8c26970e805f4
drapenchuk <drapenchuk@bloomberg.net>
2016-02-08 11:50:52 -0500

    clean up library

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -10,12 +10,11 @@ module KubernetesCookbook
     include Chef::Mixin::ShellOut
 
     def flannel_network?
-      if File.exist?('/bin/etcdctl')
-        if File.exist?('/etc/kubernetes/secrets/client.srv.crt')
-          cmd = shell_out!('etcdctl --cert-file=/etc/kubernetes/secrets/client.srv.crt --key-file=/etc/kubernetes/secrets/client.srv.key --ca-file=/etc/kubernetes/secrets/client.ca.crt get coreos.com/network/config | sed "/^$/d"', returns: [0, 2, 4])
-        else
-          cmd = shell_out!('etcdctl get coreos.com/network/config | sed "/^$/d"', returns: [0, 2, 4])
-        end
+      if File.exist?('/bin/etcdctl') && File.exist?('/etc/kubernetes/secrets/client.srv.crt')
+        cmd = shell_out!('etcdctl --cert-file=/etc/kubernetes/secrets/client.srv.crt --key-file=/etc/kubernetes/secrets/client.srv.key --ca-file=/etc/kubernetes/secrets/client.ca.crt get coreos.com/network/config | sed "/^$/d"', returns: [0, 2, 4])
+        cmd.stderr.empty? && (cmd.stdout != /^Error/)
+      elsif File.exist?('/bin/etcdctl')
+        cmd = shell_out!('etcdctl get coreos.com/network/config | sed "/^$/d"', returns: [0, 2, 4])
         cmd.stderr.empty? && (cmd.stdout != /^Error/)
       else
         puts 'etcdctl is not available'
