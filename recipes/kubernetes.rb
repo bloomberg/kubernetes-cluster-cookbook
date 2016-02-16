efd2683d404854282729e7d2fbd4cca18a961302
drapenchuk <drapenchuk@bloomberg.net>
2016-02-16 09:19:33 -0500

    tweak the kubernetes template

--- recipes/kubernetes.rb
+++ recipes/kubernetes.rb
@@ -9,8 +9,10 @@ template '/etc/kubernetes/config' do
   mode '0640'
   source 'kube-config.erb'
   variables(
+    kubernetes_log_level: node['kubernetes']['log']['level'],
     kubernetes_master: node['fqdn'],
     kubernetes_api_port: node['kubernetes']['insecure']['apiport'],
-    kubernetes_secure_api_port: node['kubernetes']['secure']['apiport']
+    kubernetes_secure_api_port: node['kubernetes']['secure']['apiport'],
+    etcd_cert_dir: node['kubernetes']['secure']['directory']
   )
 end
