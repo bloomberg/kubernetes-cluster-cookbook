0351cf3ca543da5b44c1ee737e037c1230ead2c2
Grant Ridder <shortdudey123@gmail.com>
2016-06-20 10:12:42 -0700

    Fix FC005, FC064, FC065

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,6 @@ version '1.1.0'
 %w(centos redhat).each do |name|
   supports name, '~> 7.1'
 end
+
+source_url 'https://github.com/bloomberg/kubernetes-cluster-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/bloomberg/kubernetes-cluster-cookbook/issues' if respond_to?(:issues_url)
